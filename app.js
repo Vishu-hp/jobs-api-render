fc2e144e0fc94683a55f3c102e10a1593f5d35ac
Vishu Bhardwaj <vishubhardwaj24528@gmail.com>
2025-02-08 14:30:29 +0530

    swagger ui added

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ const express = require("express");
 const app = express();
 const authMiddleware = require("./middleware/authentication");
 
-// security packages
+// extra security packages
 const helmet = require("helmet");
 const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
+
+// Swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
 
 // connect to db
 const connectDb = require("./db/connect");
@@ -34,8 +39,13 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
-// routes
+app.get("/", (req, res) => {
+  res.send("<h1>jobs api</h1><a href='/api-docs'>Documentation</a>");
+});
 
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
+// routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authMiddleware, jobsRouter);
 
